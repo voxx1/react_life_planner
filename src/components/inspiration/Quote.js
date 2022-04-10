bb5dfe94306f594a236dc6b508e94ef1c715f23c
voxx1 <61800692+voxx1@users.noreply.github.com>
2022-04-10 12:37:17 +0200

    quote loading text

--- src/components/inspiration/Quote.js
+++ src/components/inspiration/Quote.js
@@ -11,10 +11,13 @@ const Quote = () => {
         shareLink: null
     })
 
+    const [isLoading, setIsLoading] = useState(null);
+
 
 
     useEffect(() => {
         const fetchQuote = async () => {
+            setIsLoading(true)
             const response = await fetch("https://quotes.rest/qod?language=en");
             const responseData = await response.json();
             const QOD = responseData.contents.quotes[0].quote
@@ -26,8 +29,11 @@ const Quote = () => {
                 author: QODauthor,
                 shareLink: QODlink
             })
+            setIsLoading(false)
         }
+
         fetchQuote();
+
     }, [])
 
 
@@ -36,9 +42,13 @@ const Quote = () => {
             <MainHeader />
             <div className={classes.quote_form}>
                 <h1>Inspiring Quote of the day</h1>
-                <h2>{quoteInfo.text}</h2>
-                <h3>{quoteInfo.author}</h3>
-                <a href={quoteInfo.shareLink} rel="noreferrer" target="_blank"><input className="spending-form-button" type="button" value="Share link to quote" /></a>
+                {isLoading ? <p style={{ color: "white", textAlign: "center", fontSize: "20px", marginTop: "20px" }}>Just a second, your quote is loading!</p> : <>
+                    <h2>{quoteInfo.text}</h2>
+                    <h3>{quoteInfo.author}</h3>
+                    <a href={quoteInfo.shareLink} rel="noreferrer" target="_blank"><input className="spending-form-button" type="button" value="Share link to quote" /></a>
+
+                </>}
+
 
             </div>
         </>
